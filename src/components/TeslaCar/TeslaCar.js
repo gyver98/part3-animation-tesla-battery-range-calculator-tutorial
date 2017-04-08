d7a9c19dd47f61d27429595cdb5356e5799c91a1
somdari <matthewchoi7@gmail.com>
2017-04-08 22:47:27 +1000

    update TeslaCar

--- src/components/TeslaCar/TeslaCar.js
+++ src/components/TeslaCar/TeslaCar.js
@@ -3,7 +3,7 @@ import './TeslaCar.css';
 
 const TeslaCar = (props) => (
   <div className="tesla-car tesla-car-animation">
-    <div className="tesla-wheels tesla-wheels-animation">
+    <div className={`tesla-wheels tesla-wheel--animation--${props.wheelsize}`}>
       <div className={`tesla-wheel tesla-wheel--front tesla-wheel--${props.wheelsize}`}></div>
       <div className={`tesla-wheel tesla-wheel--rear tesla-wheel--${props.wheelsize}`}></div>
     </div>
