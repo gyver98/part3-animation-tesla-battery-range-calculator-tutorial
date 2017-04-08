6b2e2c4f819fd0e369c6e2bf4d2bb5fa11b4a5fd
somdari <matthewchoi7@gmail.com>
2017-04-08 21:22:05 +1000

    adding componentWillReceiveProps

--- src/components/TeslaCounter/TeslaCounter.js
+++ src/components/TeslaCounter/TeslaCounter.js
@@ -1,37 +1,63 @@
 import React from 'react';
 import './TeslaCounter.css';
 
-const TeslaCounter = ({ initValues, currentValue, increment, decrement }) => (
-  <div className="tesla-counter">
-    <p className="tesla-counter__title">{initValues.title}</p>
-    <div className="tesla-counter__container cf">
-      <div className="tesla-counter__item">
-        <div className="flip-in-hor-top">
-          <p className="tesla-counter__number ">
-            { currentValue }
-            <span>{ initValues.unit }</span>
-          </p>
-        </div>
-        <div className="tesla-counter__controls">
-          <button 
-            onClick={(e) => {
-              e.preventDefault();
-              increment(currentValue)}} 
-            disabled={currentValue >= initValues.max} 
-          >
-          </button>
-          <button 
-            onClick={(e) => {
-              e.preventDefault();
-              decrement(currentValue)}} 
-            disabled={currentValue <= initValues.min} 
-          >
-          </button>
+class TeslaCounter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      direction: ''
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { currentValue, initValues } = nextProps;
+    if (this.props.currentValue !== nextProps.currentValue) {
+      const direction = this.props.currentValue < nextProps.currentValue ? 'increase' : 'decrese';
+      this.setState({
+        direction
+      });
+      return;  
+    } 
+  }
+  
+  render() {
+    const { initValues, currentValue, increment, decrement } = this.props;
+    const { direction } = this.state;
+    const animationClass = direction==='increase'? 'flip-in-hor-top' : 'flip-in-hor-bottom';
+    
+    return (
+      <div className="tesla-counter">
+        <p className="tesla-counter__title">{initValues.title}</p>
+        <div className="tesla-counter__container cf">
+          <div className="tesla-counter__item">
+            <div className={`${animationClass}`}>
+              <p className="tesla-counter__number ">
+                { currentValue }
+                <span>{ initValues.unit }</span>
+              </p>
+            </div>
+            <div className="tesla-counter__controls">
+              <button 
+                onClick={(e) => {
+                  e.preventDefault();
+                  increment(currentValue)}} 
+                disabled={currentValue >= initValues.max} 
+              >
+              </button>
+              <button 
+                onClick={(e) => {
+                  e.preventDefault();
+                  decrement(currentValue)}} 
+                disabled={currentValue <= initValues.min} 
+              >
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>  
-);
+    )    
+  }
+};
 
 TeslaCounter.propTypes = {
   currentValue: React.PropTypes.number,
